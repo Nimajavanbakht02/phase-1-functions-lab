1c1e44b527ff77a836f6667a98ab6fef4077e191
NimaJavanbakht <nimabrah19@gmail.com>
2024-01-29 15:20:15 -0800

    adds distance tracker

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    let hqDistance = someValue - 42;
+    return Math.abs(hqDistance);
+}
+
+function distanceFromHqInFeet(someValue) {
+    let hQInFeet = distanceFromHqInBlocks(someValue) * 264
+    return hQInFeet;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let travelledInFeet = Math.abs(destination - start) * 264
+    return travelledInFeet;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distanceInFeet = distanceTravelledInFeet(start, destination);
+    if (distanceInFeet <= 400)
+        return 0;
+    if  (distanceInFeet > 400 && distanceInFeet < 2000)
+        return (distanceInFeet - 400) * 0.02
+    if (distanceInFeet > 2000 && distanceInFeet < 2500)
+        return 25
+    if (distanceInFeet > 2500)
+        return "cannot travel that far"
+}
